7774551f96fcb075d632f7258f16d6f9b24e6c20
silent00knight <hrgupta19@gmail.com>
2024-07-18 16:28:38 +0530

    update script

--- loader.js
+++ loader.js
@@ -1,8 +1,8 @@
-var loading = gsap.timeline();
-loading.to(".loader",{
-    top: "-120%",
-    backgroundColor : "#dadada6d",
-    delay: 11,
-    duration: 1.25,
-    opacity: 0,
-})
+// var loading = gsap.timeline();
+// loading.to(".loader",{
+//     top: "-120%",
+//     backgroundColor : "#dadada6d",
+//     delay: 11,
+//     duration: 1.25,
+//     opacity: 0,
+// })
